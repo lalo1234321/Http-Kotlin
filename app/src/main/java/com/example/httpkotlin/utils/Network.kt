38696d97c49f9dc6f2c053a05c9e38e72f85164f
lalo1234321 <eduardodominguezdev@gmail.com>
2022-09-23 16:28:14 -0500

    configuring manifest and class for Network functionalities

---
+++ app/src/main/java/com/example/httpkotlin/utils/Network.kt
@@ -0,0 +1,10 @@
+package com.example.httpkotlin.utils
+
+class Network {
+    //definir una clase estatica en kotlin
+    companion object {
+        fun validarRed():Boolean {
+            return true
+        }
+    }
+}
